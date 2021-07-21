7f3f1d55402b9fc605ba5b2d73f21b1659070d52
Andrew Giang <giang184@gmail.com>
2021-07-21 09:07:32 -0700

    add song.rb

---
+++ lib/song.rb
@@ -0,0 +1,11 @@
+class Song
+
+  def self.find_by_album(alb_id)
+    songs = []
+    @@songs.values.each do |song|
+      if song.album_id == alb_id
+        songs.push(song)
+      end
+    end
+    songs
+  end

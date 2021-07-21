282b65a2208ad1cb1b3a0c1fdc5360133946dce6
Andrew Giang <giang184@gmail.com>
2021-07-21 09:45:20 -0700

    add capybara test spec

--- spec/album_integration_spec.rb
+++ spec/album_integration_spec.rb
@@ -12,3 +12,14 @@ describe('create an album path', {:type => :feature}) do
     expect(page).to have_content('Yellow Submarine')
   end
 end
+
+describe('create a song path', {:type => :feature}) do
+  it('creates an album and then goes to the album page') do
+    album = Album.new("Yellow Submarine", nil)
+    album.save
+    visit("/albums/#{album.id}")
+    fill_in('song_name', :with => 'All You Need Is Love')
+    click_on('Add song')
+    expect(page).to have_content('All You Need Is Love')
+  end
+end
